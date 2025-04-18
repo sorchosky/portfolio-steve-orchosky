1a35f4e951005cd6c7f5ebd02432ebf8396ff0e1
Steve <sorchosky@gmail.com>
2025-04-18 09:42:37 -0500

    Updating with browsersync

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Steve's Portfolio
+
+Use npm install to set up locally
+Use npm run run to launch localhost:3000 and initialize watch tasks
--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,16 @@ const sass = require('gulp-sass')(require('sass'));
 let concat = require('gulp-concat');
 let autoprefixer = require('gulp-autoprefixer');
 let fileinclude = require('gulp-file-include');
+const browserSync = require('browser-sync').create();
  
-sass.compiler = require('node-sass');
+sass.compiler = require('sass');
+
+// Static Server + watching scss/html files
+gulp.task('serve', function() {
+  browserSync.init({
+      server: "./app"
+  });
+});
 
 gulp.task('html', function () {
   return gulp.src('./src/html/pages/*.html')
@@ -15,6 +23,7 @@ gulp.task('html', function () {
       basepath: '@file'
     }))
     .pipe(gulp.dest('./build'))
+    .pipe(browserSync.stream());
 });
  
 gulp.task('sass', function () {
@@ -23,16 +32,21 @@ gulp.task('sass', function () {
     .pipe(autoprefixer({
       cascade: false
     }))
-    .pipe(gulp.dest('./build'));
+    .pipe(gulp.dest('./build'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('scripts', function () {
   return gulp.src('./src/js/*.js')
   .pipe(concat('scripts.js'))
   .pipe(gulp.dest('./build'))
+  .pipe(browserSync.stream());
 });
  
 gulp.task('default', function () {
+  browserSync.init({
+      server: "./build"
+  });
   gulp.watch('./src/html/**/*.html', gulp.series('html'));
   gulp.watch('./src/scss/**/*.scss', gulp.series('sass'));
   gulp.watch('./src/js/*.js', gulp.series('scripts'));
